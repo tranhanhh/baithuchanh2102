599685c4f4a061c35fe23fcc9dabe4b7df36ab39
tranhanhh <tranthihanh190102@gmail.com>
2022-02-21 18:44:13 +0700

    first commit

---
+++ src/App51.java
@@ -0,0 +1,10 @@
+public class App51 {
+
+   public static void main(String[] args) throws Exception {
+    int n =0;
+    for (; n < 5;) {
+        System.out.println(n);
+        n++;
+        } 
+    }
+}
